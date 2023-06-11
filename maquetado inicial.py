394dda8bf2cf66c32858d47ce607db247ac3e0d2
Débora Vizama Infante <vizamadebora154@gmail.com>
2023-06-11 14:13:12 -0400

    incrustacion de tk con pygame, aunque solo muestra una imagen.

--- maquetado inicial.py
+++ maquetado inicial.py
@@ -1,123 +1,70 @@
+import tkinter as tk
 import pygame
-from tkinter import *
-from matplotlib import pyplot as plt
+from PIL import Image, ImageTk
 
-# Configuración de la ventana de pygame
-WIDTH, HEIGHT = 800, 600
-FPS = 60
-
-# Configuración de la ventana de Tkinter
-ventana = Tk()
-ventana.title("Simulación de Movimiento Rectilíneo Uniformemente Acelerado")
-ventana.geometry("900x600")
-ventana.config(bg="RoyalBlue4")
-
-# Variables de movimiento
-velocidad_inicial = 0
-aceleracion = 0
-
-# Función para iniciar la simulación
-def start_simulation():
-    global velocidad_inicial, aceleracion
-    velocidad_inicial = float(entry_velocidad.get())
-    aceleracion = float(aceleracion_entry.get())
-
-    # Configuración de la ventana de pygame
-    pygame.init()
-    clock = pygame.time.Clock()
-    ventana = pygame.display.set_mode((WIDTH, HEIGHT))
-    pygame.display.set_caption("Simulación de Movimiento Rectilíneo Uniformemente Acelerado")
-
-    tiempo_simulacion = 10  # Tiempo total de la simulación en segundos
-    time_interval = 1 / FPS
-
-    simulation = MRUASimulation(velocidad_inicial, aceleracion)
-
-    running = True
-    posicion = []
-    tiempo = []
-    elapsed_time = 0
-
-    while running:
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                running = False
-
-        # Actualizar y dibujar la simulación
-        simulation.update(time_interval)
-        posicion.append(simulation.position)
-        tiempo.append(elapsed_time)
-        elapsed_time += time_interval
-
-        ventana.fill((0, 0, 0))
-        start_simulation(ventana, simulation)
-
-        pygame.display.flip()
-        clock.tick(FPS)
-
-        # Detener la simulación después de cierto tiempo
-        if elapsed_time >= tiempo_simulacion:
-            running = False
-
-    # Mostrar el gráfico al final de la simulación
-    crear_grafico(posicion, tiempo)
-
-    pygame.quit()
-
-# Clase para la simulación del movimiento
-class MRUASimulation:
-    def __init__(self, initial_velocity, acceleration):
-        self.initial_velocity = initial_velocity
-        self.acceleration = acceleration
-        self.time = 0
-        self.position = 0
-
-    def update(self, dt):
-        self.position = self.initial_velocity * self.time + 0.5 * self.acceleration * self.time**2
-        self.time += dt
-
-def crear_grafico(posicion, tiempo):
-    plt.plot(tiempo, posicion)
-    plt.xlabel('Tiempo (s)')
-    plt.ylabel('Posición (m)')
-    plt.title('Simulación de Movimiento Rectilíneo Uniforme Acelerado')
-    plt.grid(True)
-    plt.show()
-
-# Etiquetas y campos de entrada de Tkinter
-frame_1 = Frame(ventana, bg="steel blue", width=300, height=600, border=15, relief=SUNKEN)
-frame_1.pack(side=LEFT)
-
-label_velocidad = Label(frame_1, text="Velocidad Inicial: ", bg="steel blue")
-label_velocidad.pack()
-label_velocidad.place(x=35, y=200)
-
-label_aceleracion = Label(frame_1, text="Aceleración: ", bg="steel blue")
-label_aceleracion.pack()
-label_aceleracion.place(x=57, y=160)
-
-entry_velocidad = Entry(frame_1)
-entry_velocidad.pack()
-entry_velocidad.place(x=130, y=200)
-
-aceleracion_entry = Entry(frame_1)
-aceleracion_entry.pack()
-aceleracion_entry.place(x=130, y=160)
-
-frame_2 = Frame(ventana, bg="SteelBlue4", width=600, height=150, border=15, relief=SUNKEN)
-frame_2.pack(side=BOTTOM)
-
-frame_3 = Frame(ventana, bg="SteelBlue4", width=600, height=450, border=15, relief=SUNKEN)
-frame_3.pack(side=TOP)
-
-position_label = Label(frame_2, text='Posición: 0')
-position_label.pack()
-
-time_label = Label(frame_2, text='Tiempo: 0')
-time_label.pack()
-
-boton_inicio = Button(frame_2, text="INICIO", command=start_simulation, bg="powder blue", relief=SUNKEN)
-boton_inicio.pack()
-boton_inicio.place(x=90, y=270, width=120, height=60)
-
-ventana.mainloop()
+class MRUASimulacion:
+    def __init__(self, ventana):
+        self.ventana = ventana
+        self.ventana.title("Simulación MRUA")
+        
+        self.velocidad = 0
+        self.aceleracion = 0
+        self.tiempo = 0
+        self.posicion = 0
+        
+        self.crear_widgets()
+        
+    def crear_widgets(self):
+        # Frame de entrada
+        entrada = tk.Frame(self.ventana)
+        entrada.pack(pady=10)
+        
+        # Etiquetas y campos de entrada
+        tk.Label(entrada, text="Velocidad inicial (m/s):").grid(row=0, column=0, padx=5, pady=5)
+        self.velocidad_entrada = tk.Entry(entrada)
+        self.velocidad_entrada.grid(row=0, column=1, padx=5, pady=5)
+        
+        tk.Label(entrada, text="Aceleración (m/s^2):").grid(row=1, column=0, padx=5, pady=5)
+        self.aceleracion_entrada = tk.Entry(entrada)
+        self.aceleracion_entrada.grid(row=1, column=1, padx=5, pady=5)
+        
+        tk.Label(entrada, text="Tiempo (s):").grid(row=2, column=0, padx=5, pady=5)
+        self.tiempo_entrada = tk.Entry(entrada)
+        self.tiempo_entrada.grid(row=2, column=1, padx=5, pady=5)
+        
+        # Botón de simulación
+        simulacion = tk.Button(self.ventana, text="Simular", command=self.simulacion)
+        simulacion.pack(pady=10)
+        
+        # Widget de imagen
+        self.image_label = tk.Label(self.ventana)
+        self.image_label.pack(pady=10)
+        
+    def simulacion(self):
+        try:
+            self.velocidad = float(self.velocidad_entrada.get())
+            self.aceleracion = float(self.aceleracion_entrada.get())
+            self.time = float(self.tiempo_entrada.get())
+            
+            self.posicion = self.velocidad * self.tiempo + 0.5 * self.aceleracion * self.tiempo**2
+            self.velocidad += self.aceleracion * self.tiempo
+            
+            pygame.init()
+            surface = pygame.Surface((600, 600))
+            pygame.draw.circle(surface, (255, 0, 0), (50, 50), 20)
+            py_Mage = pygame.image.tostring(surface, "RGB")
+            
+            imagen = Image.frombytes("RGB", surface.get_size(), py_Mage)
+            foto = ImageTk.PhotoImage(imagen)
+            
+            self.image_label.config(image=foto)
+            self.image_label.image = foto
+            
+            pygame.quit()
+        except ValueError:
+            self.image_label.config(image=None)
+        
+if __name__ == "__main__":
+    ventana = tk.Tk()
+    mrua = MRUASimulacion(ventana)
+    ventana.mainloop()
