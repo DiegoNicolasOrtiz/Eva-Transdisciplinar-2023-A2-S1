b1defd640aa6d3c3277b460c68f032a531604177
Débora Vizama Infante <vizamadebora154@gmail.com>
2023-06-14 23:17:25 -0400

    grafica con la libreria matplotlib

--- maquetado inicial.py
+++ maquetado inicial.py
@@ -1,70 +1,22 @@
-import tkinter as tk
-import pygame
-from PIL import Image, ImageTk
+import matplotlib.pyplot as plt
 
-class MRUASimulacion:
-    def __init__(self, ventana):
-        self.ventana = ventana
-        self.ventana.title("Simulación MRUA")
-        
-        self.velocidad = 0
-        self.aceleracion = 0
-        self.tiempo = 0
-        self.posicion = 0
-        
-        self.crear_widgets()
-        
-    def crear_widgets(self):
-        # Frame de entrada
-        entrada = tk.Frame(self.ventana)
-        entrada.pack(pady=10)
-        
-        # Etiquetas y campos de entrada
-        tk.Label(entrada, text="Velocidad inicial (m/s):").grid(row=0, column=0, padx=5, pady=5)
-        self.velocidad_entrada = tk.Entry(entrada)
-        self.velocidad_entrada.grid(row=0, column=1, padx=5, pady=5)
-        
-        tk.Label(entrada, text="Aceleración (m/s^2):").grid(row=1, column=0, padx=5, pady=5)
-        self.aceleracion_entrada = tk.Entry(entrada)
-        self.aceleracion_entrada.grid(row=1, column=1, padx=5, pady=5)
-        
-        tk.Label(entrada, text="Tiempo (s):").grid(row=2, column=0, padx=5, pady=5)
-        self.tiempo_entrada = tk.Entry(entrada)
-        self.tiempo_entrada.grid(row=2, column=1, padx=5, pady=5)
-        
-        # Botón de simulación
-        simulacion = tk.Button(self.ventana, text="Simular", command=self.simulacion)
-        simulacion.pack(pady=10)
-        
-        # Widget de imagen
-        self.image_label = tk.Label(self.ventana)
-        self.image_label.pack(pady=10)
-        
-    def simulacion(self):
-        try:
-            self.velocidad = float(self.velocidad_entrada.get())
-            self.aceleracion = float(self.aceleracion_entrada.get())
-            self.time = float(self.tiempo_entrada.get())
-            
-            self.posicion = self.velocidad * self.tiempo + 0.5 * self.aceleracion * self.tiempo**2
-            self.velocidad += self.aceleracion * self.tiempo
-            
-            pygame.init()
-            surface = pygame.Surface((600, 600))
-            pygame.draw.circle(surface, (255, 0, 0), (50, 50), 20)
-            py_Mage = pygame.image.tostring(surface, "RGB")
-            
-            imagen = Image.frombytes("RGB", surface.get_size(), py_Mage)
-            foto = ImageTk.PhotoImage(imagen)
-            
-            self.image_label.config(image=foto)
-            self.image_label.image = foto
-            
-            pygame.quit()
-        except ValueError:
-            self.image_label.config(image=None)
-        
-if __name__ == "__main__":
-    ventana = tk.Tk()
-    mrua = MRUASimulacion(ventana)
-    ventana.mainloop()
+def movimiento_rectilineo_uniformemente_acelerado(v0, a, t):
+    # Calcula la posición en función del tiempo
+    x = v0 * t + 0.5 * a * t**2
+    return x
+
+# Valores iniciales
+v0 = 0  # Velocidad inicial en m/s
+a = 2   # Aceleración constante en m/s^2
+t = range(0, 11)  # Lista de tiempos de 0 a 10 segundos
+
+# Calcula las posiciones correspondientes a cada tiempo
+posiciones = [movimiento_rectilineo_uniformemente_acelerado(v0, a, tiempo) for tiempo in t]
+
+# Grafica la posición en función del tiempo
+plt.plot(t, posiciones)
+plt.xlabel('Tiempo (s)')
+plt.ylabel('Posición (m)')
+plt.title('Movimiento rectilíneo uniformemente acelerado')
+plt.grid(True)
+plt.show()
