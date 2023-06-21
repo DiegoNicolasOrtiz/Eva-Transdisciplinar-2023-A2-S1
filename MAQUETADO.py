a211e13fbfd7afd664d927758c41166ac036831a
DiegoNicolasOrtiz <diego.ortiz2023@alu.uct.cl>
2023-06-21 13:03:45 -0400

    Incrustración de la ventana de Pygame para proyectar en la interfaz la simulación.

--- MAQUETADO.py
+++ MAQUETADO.py
@@ -6,7 +6,11 @@ import pygame
 from pygame.locals import *
 from PIL import Image, ImageTk
 
+WIDTH, HEIGHT = 665, 435
+FPS = 60
+
 #--------------------------------------tkinter---------------------------------------------------------------------
+pygame.init()
 
 ventana = Tk()
 
@@ -14,7 +18,7 @@ frame = Frame(ventana)
 frame.pack()
 ventana.title("Proyecto")#El titulo modificado
 
-ventana.geometry("900x600")#Dimension 
+ventana.geometry("1150x800")#Dimension 
 
 ventana.config(bg="RoyalBlue4")#Color de fondo
 
@@ -25,9 +29,9 @@ frame_1.pack()
 
 frame_1.config(bg = "steel blue")
 
-frame_1.config(width ="300", height ="600")    
+frame_1.config(width ="300", height ="300")    
 
-frame_1.pack(side=LEFT)
+frame_1.place(x=30, y=200)
 
 frame_1.config(border = "15")
 
@@ -38,25 +42,28 @@ frame_1.config(relief = SUNKEN)
 label_texto = Label(ventana, text = """Movimiento Rectilíneo Uniformemente Acelerado
 (MRUA)""")
 label_texto.pack()
-label_texto.place(x=19, y=55)
+label_texto.config(border = "7")
+label_texto.config(relief = SUNKEN)
+label_texto.place(x=47, y=95)
+
 
 label_velocidad = Label(ventana, text = "Velocidad Inicial: ", bg="steel blue")
 label_velocidad.pack()
-label_velocidad.place(x=35, y=200)
+label_velocidad.place(x=55, y=300)
 
 label_aceleracion = Label(ventana, text = "Aceleración: ", bg="steel blue")
 label_aceleracion.pack()
-label_aceleracion.place(x=57, y=160)
+label_aceleracion.place(x=77, y=260)
 
 #----------------------------Entrys--------------------------------------------------------------
 
 entry_velocidad = Entry(ventana)
 entry_velocidad.pack()
-entry_velocidad.place(x=130, y=200)
+entry_velocidad.place(x=150, y=300)
 
 aceleracion_entry = Entry(ventana)
 aceleracion_entry.pack()
-aceleracion_entry.place(x=130, y=160)
+aceleracion_entry.place(x=150, y=260)
 
 #---------------------------FRAME 2-------------------------------------------------------------------------
 
@@ -65,9 +72,9 @@ frame_2.pack()
 
 frame_2.config(bg = "SteelBlue4")
 
-frame_2.config(width ="600", height ="150")    
+frame_2.config(width ="450", height ="100")    
 
-frame_2.pack(side = BOTTOM)
+frame_2.place(x=510, y=520)
 
 frame_2.config(border = "15")
 
@@ -80,22 +87,20 @@ frame_3.pack()
 
 frame_3.config(bg = "SteelBlue4")
 
-frame_3.config(width ="600", height ="450")    
+frame_3.config(width ="700", height ="470")    
 
-frame_3.pack(side = TOP)
+frame_3.place(x=370, y=30)
 
 frame_3.config(border = "15")
 
 frame_3.config(relief = SUNKEN)
 
-canvas_simulation = tkinter.Canvas(ventana, width=400, height=450, bg="black")
-canvas_simulation.pack(pady=10)
-canvas_simulation.place(x=300, y=10)
+label = tkinter.Label(ventana)
+label.place(x=385, y=45)
+
 #--------------------------------------------pygame--------------------------------------------------
 
 # Configuración de la ventana de pygame
-WIDTH, HEIGHT = 800, 600
-FPS = 60
 
 # Clase para la simulación del movimiento
 class MRUASimulation:
@@ -108,11 +113,13 @@ class MRUASimulation:
 
     def update(self, dt):
         self.position = self.initial_velocity * self.time + 0.5 * self.acceleration * self.time**2
-        self.positiony = -1*(self.initial_velocity * self.time + 0.5 * self.acceleration * self.time**3)
+        self.positiony = -1*(self.initial_velocity * self.time + 0.5 * self.acceleration * self.time**2)
         self.time += dt
 
+simulation = MRUASimulation(0, 0)
 # Función para dibujar la tabla gráfica en pygame
 def draw_graph(surface, simulation):
+    surface.fill((0, 0, 0))
     pygame.draw.line(surface, (130, 160, 200), (WIDTH // 4 - 30, 0), (WIDTH // 4 - 30, HEIGHT), 1)
     pygame.draw.line(surface, (130, 160, 200), (WIDTH // 4 - 60, 0), (WIDTH // 4 - 60, HEIGHT), 1)
     pygame.draw.line(surface, (130, 160, 200), (WIDTH // 4 - 90, 0), (WIDTH // 4 - 90, HEIGHT), 1)
@@ -157,41 +164,41 @@ def draw_graph(surface, simulation):
     pygame.draw.line(surface, (130, 160, 200), (0, HEIGHT//2 - 210), (WIDTH, HEIGHT//2 - 210), 1)
     pygame.draw.line(surface, (130, 160, 200), (0, HEIGHT//2 - 240), (WIDTH, HEIGHT//2 - 240), 1)
     pygame.draw.line(surface, (130, 160, 200), (0, HEIGHT//2 - 270), (WIDTH, HEIGHT//2 - 270), 1)
-    pygame.draw.line(surface, (255, 255, 255), (0, 450), (WIDTH, 450), 3)
+    pygame.draw.line(surface, (255, 255, 255), (0, 330), (WIDTH, 330), 3)
     pygame.draw.line(surface, (255, 255, 255), (WIDTH // 4, 0), (WIDTH // 4, HEIGHT), 3)
-    pygame.draw.circle(surface, (255, 0, 0), (WIDTH // 4 + int(simulation.time*100), 450 + int(simulation.positiony)), 5)
+    pygame.draw.circle(surface, (255, 0, 0), (WIDTH // 4 + int(simulation.time)*30, 330 + int(simulation.positiony)), 5)
+
+    pygame_image = pygame.image.tostring(surface, 'RGB')
+    image = Image.frombytes('RGB', surface.get_size(), pygame_image)
+    photo = ImageTk.PhotoImage(image)
+    label.config(image=photo)
+    label.image = photo
+
+def iniciar():
+    
+    initial_velocity = float(entry_velocidad.get())
+    acceleration = float(aceleracion_entry.get())
+    simulation.acceleration = acceleration
+    simulation.initial_velocity = initial_velocity
+    simulation.update(1)
+    print(simulation.time)
+    surf = pygame.Surface((WIDTH,HEIGHT))
+    draw_graph(surf, simulation)
+    # Programar la próxima actualización
+    ventana.after(5000//15, iniciar)
+
 # Función para iniciar la simulación
 def start_simulation():
     initial_velocity = float(entry_velocidad.get())
     acceleration = float(aceleracion_entry.get())
-    simulation = MRUASimulation(initial_velocity, acceleration)
-    
+    surf = pygame.Surface((WIDTH,HEIGHT))
+
     # Configuración de la ventana de pygame
-    pygame.init()
-    clock = pygame.time.Clock()
-    surface = pygame.display.set_mode((WIDTH, HEIGHT))
-    pygame.display.set_caption("Simulación de Movimiento Rectilíneo Uniformemente Acelerado")
-
-    running = True
-    while running:
-        for event in pygame.event.get():
-            if event.type == QUIT:
-                running = False
-
-        surface.fill((0, 0, 0))
-
-        # Actualizar y dibujar la simulación
-        simulation.update(1 / FPS)
-        draw_graph(surface, simulation)
-
-        pygame.display.flip()
-        clock.tick(FPS)
-        
-    pygame.quit()
+    iniciar()
 
 boton_inicio = Button(ventana, text = "Iniciar Simulación", command = start_simulation, bg = "powder blue")#BOTON
 boton_inicio.config(relief=SUNKEN)
 boton_inicio.pack()
-boton_inicio.place(x = 90, y = 270, width = 160, height = 60)#Pocision del boton
+boton_inicio.place(x = 100, y = 370, width = 160, height = 60)#Pocision del boton
 
 ventana.mainloop()
